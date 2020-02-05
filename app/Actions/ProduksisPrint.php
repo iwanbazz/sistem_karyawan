ebaa7aa5d012142fdeb9f86ce8f611e104596dcd
dwiprasetiawan <dwiprasetiawan2@gmail.com>
2020-02-06 01:16:48 +0700

    Add more actions

---
+++ app/Actions/ProduksisPrint.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Actions;
+
+use TCG\Voyager\Actions\AbstractAction;
+
+class ProduksisPrint extends AbstractAction
+{
+    public function getTitle()
+    {
+        return 'Print';
+    }
+
+    public function getIcon()
+    {
+        return 'voyager-file-text';
+    }
+
+    public function getPolicy()
+    {
+        return 'read';
+    }
+
+    public function getAttributes()
+    {
+        return [
+            'class' => 'btn btn-sm btn-primary pull-right',
+        ];
+    }
+
+    public function getDefaultRoute()
+    {
+        return route('voyager.produksis.index');
+    }
+
+    public function shouldActionDisplayOnDataType()
+    {
+        return $this->dataType->slug == 'produksis';
+    }
+}
